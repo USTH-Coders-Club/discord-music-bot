c0ee3d2f9e2a4385f140cdbcf6b057b52322a3a6
fishe <donhatthanh1805@gmail.com>
2022-04-15 22:09:54 +0700

    chay nhac basic

--- handler/handle.js
+++ handler/handle.js
@@ -1,15 +1,32 @@
-const {Player} = require('discord-player');
-
+require('dotenv').config();
 module.exports = {
-    async handle(client, message) {
-        console.log(message);
+    async handle(client, message, player) {
         const raw_message = message.content;
         if (!raw_message.startsWith(process.env.PREFIX) || message.author.bot){
             return;
         }
         const command =  raw_message.split(" ")[0].substring(process.env.PREFIX.length);
         const args = raw_message.split(" ").slice(1).join(" ");
-        console.log(command);
         
+        const queue = player.createQueue(message.guildId, {
+            metadata: {
+                channel: "964542085195767878"
+            }
+        });
+        try {
+            if (!queue.connection) await queue.connect('916687023253893140');
+        } catch {
+            queue.destroy();
+            return await message.channel.send({ content: "Could not join your voice channel!", ephemeral: true });
+        }
+
+        const track = await player.search(args, {
+            requestedBy: message.author.username
+        }).then(x => x.tracks[0]);
+
+        queue.play(track);
+
+        //console.log(message);
+
     }
 }
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const Discord =  require('discord.js');
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Player } = require('discord-player');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 const Handler = require('./handler/handle');
+const player = new Player(client);
 
 client.on('ready', () => {
     console.log("Bot logged in.");
 })
 
 client.on('messageCreate', (message) => {
-    Handler.handle(client, message);
+    Handler.handle(client, message, player);
 })
 
 client.login(process.env.CLIENT_TOKEN);
